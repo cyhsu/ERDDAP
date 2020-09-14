122841244b0fbba5c468aa79b3325f05491a0942
cyhsu <chsu1@tamu.edu>
2020-09-14 01:29:03 -0500

    tomcat8 is running, but the APP (ERDDAP) is still unable to see on the browser although I can see it within the container

--- Dockerfile
+++ Dockerfile
@@ -3,27 +3,28 @@ MAINTAINER Franke Hsu "chsu1@tamu.edu"
 
 ENV HOME /usr/local/tomcat8
 
-#RUN apt-get -y update && apt-get -y upgrade 
-RUN apt-get -y update && apt-get -y install unzip wget openjdk-8-jdk
+RUN apt-get -y update #&& apt-get -y upgrade
+RUN apt-get -y install unzip wget vim w3m openjdk-8-jdk
 
-#RUN apt-get -y install apt-utils unzip wget openjdk-8-jdk
-#RUN apt install unzip wget w3m lsof openjdk-8-jdk -y
-##
-##COPY ./installERDDAP.sh $HOME/installERDDAP.sh
-##
-##COPY ./doc $HOME/doc
+ENV ERDDAP_VERSION=8.5.57
 
-COPY . $HOME/
+ENV TOMCAT_NAME=apache-tomcat-${ERDDAP_VERSION}.tar.gz
 
-RUN chmod +x $HOME/installERDDAP.sh
+ENV ERDDAP_URL https://ftp.wayne.edu/apache/tomcat/tomcat-8/v${ERDDAP_VERSION}/bin/${TOMCAT_NAME} 
 
 WORKDIR $HOME
 
-RUN mkdir happy
+RUN wget ${ERDDAP_URL} -O ${TOMCAT_NAME}
 
-RUN ./installERDDAP.sh
-#RUN $HOME/installERDDAP.sh
+RUN tar -xf ${TOMCAT_NAME} -C $HOME --strip 1 && rm ${TOMCAT_NAME}
+
+COPY ./doc $HOME/doc
+
+RUN cp ./doc/bin/setenv.sh ./bin/setenv.sh
+RUN cp ./doc/conf/context.xml ./conf/context.xml
+RUN cp ./doc/conf/server.xml ./conf/server.xml
+RUN cp ./doc/conf/web.xml ./conf/web.xml
 
 EXPOSE 8080 8443 8009
-#ENTRYPOINT ["tail", "-f", "/dev/null"] 
-CMD ["catalina.sh","run"]
+
+CMD ["service","/usr/local/tomcat8/bin/catalina.sh","run"]
--- installERDDAP.sh
+++ installERDDAP.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
-homedir='/usr/local/tomcat8'
-echo $PWD >> /usr/local/tomcat8/abccc
+homedir=$PWD
 
-cd $homedir
-
-mkdir tarz
-#-
-#- TOMCAT 8
-#-
-wget -q https://ftp.wayne.edu/apache/tomcat/tomcat-8/v8.5.57/bin/apache-tomcat-8.5.57.tar.gz -O ./tarz/apache-tomcat-8.5.57.tar.gz
-tar -xf ./tarz/apache-tomcat-8.5.57.tar.gz -C /usr/local/tomcat8 --strip 1
-
+cd $homedir && mkdir tarz
 #-
 #- ERDDAP v2.02
 #- 
-wget -q https://github.com/BobSimons/erddap/releases/download/v2.02/erddapContent.zip -O ./tarz/erddapContent.zip
-unzip ./tarz/erddapContent.zip
+wget -qq https://github.com/BobSimons/erddap/releases/download/v2.02/erddapContent.zip -O ./tarz/erddapContent.zip
+unzip ./tarz/erddapContent.zip -d ${homedir}/
 
-wget -q https://github.com/BobSimons/erddap/releases/download/v2.02/erddap.war
-mv ./erddap.war webapps
+#wget -qq https://github.com/BobSimons/erddap/releases/download/v2.02/erddap.war -O ./tarz/erddap.war
+#unzip ./tarz/erddap.war -d ${homedir}/erddap
+wget -qq https://github.com/BobSimons/erddap/releases/download/v2.02/erddap.war
 
-cp ./doc/bin/setenv.sh ./bin/setenv.sh
-cp ./doc/conf/context.xml ./conf/context.xml
-cp ./doc/conf/server.xml ./conf/server.xml
-cp ./doc/conf/web.xml ./conf/web.xml
-cp ./doc/content/datasets.xml ./content/erddap/datasets.xml
-cp ./doc/content/setup.xml ./content/erddap/setup.xml
+rm -rf ${homedir}/tarz
+
+cp ./doc/content/datasets.xml ${homedir}/content/erddap/datasets.xml
+cp ./doc/content/setup.xml ${homedir}/content/erddap/setup.xml
 
 #-
 #- mkdir a data folder under $HOME/data
 #-
 mkdir data
 
-ln ./bin/catalina.sh catalina.sh
+#- 
+#- Build ERDDAP Docker Container
+docker-compose up 
